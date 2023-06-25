0d97f2f1fcf177c98b3499bb714d84dd3a47fa66
ditto <amiswien@gmail.com>
2023-06-25 13:41:58 +0200

    add anomaly scores in evaluations

---
+++ engine_evaluate_mae.py
@@ -0,0 +1,135 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# BEiT: https://github.com/microsoft/unilm/tree/master/beit
+# --------------------------------------------------------
+
+import math
+import sys
+from typing import Iterable, Optional
+
+import torch
+
+import util.misc as misc
+import util.lr_sched as lr_sched
+
+from sklearn.metrics import roc_auc_score
+
+def train_one_epoch(model: torch.nn.Module,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                        device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
+                    log_writer=None, args=None):
+    model.train(True)
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 20
+
+    accum_iter = args.accum_iter
+
+    optimizer.zero_grad()
+
+    if log_writer is not None:
+        print('log_dir: {}'.format(log_writer.log_dir))
+
+    for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+
+        # we use a per iteration (instead of per epoch) lr scheduler
+        if data_iter_step % accum_iter == 0:
+            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+
+        samples = samples.to(device, non_blocking=True)
+
+        with torch.cuda.amp.autocast():
+            loss, _, _ = model(samples, mask_ratio=args.mask_ratio)
+
+        loss_value = loss.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+
+        loss /= accum_iter
+        loss_scaler(loss, optimizer, clip_grad=max_norm,
+                    parameters=model.parameters(), create_graph=False,
+                    update_grad=(data_iter_step + 1) % accum_iter == 0)
+        if (data_iter_step + 1) % accum_iter == 0:
+            optimizer.zero_grad()
+
+        torch.cuda.synchronize()
+
+        metric_logger.update(loss=loss_value)
+        min_lr = 10.
+        max_lr = 0.
+        for group in optimizer.param_groups:
+            min_lr = min(min_lr, group["lr"])
+            max_lr = max(max_lr, group["lr"])
+
+        metric_logger.update(lr=max_lr)
+
+        loss_value_reduce = misc.all_reduce_mean(loss_value)
+        if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+            """ We use epoch_1000x as the x-axis in tensorboard.
+            This calibrates different curves when batch size changes.
+            """
+            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+            log_writer.add_scalar('loss', loss_value_reduce, epoch_1000x)
+            log_writer.add_scalar('lr', max_lr, epoch_1000x)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+@torch.no_grad()
+def evaluate(data_loader, model, device):
+
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    # switch to evaluation mode
+    model.eval()
+
+    # for batch in metric_logger.log_every(data_loader, 10, header):
+    for batch in data_loader:
+        images = batch[0]
+        images = images.to(device, non_blocking=True)
+
+        # compute output
+        for i in range(32):
+            with torch.cuda.amp.autocast():
+                loss, error_map, _, _ = model(images)
+            if i == 0:
+                error_map_sum = error_map
+            else:
+                error_map_sum += error_map
+        error_map = error_map_sum / 32
+
+        metric_logger.update(loss=loss.item())
+
+        try:
+            error_maps = torch.cat((error_maps, error_map), dim=0)
+        except NameError:
+            error_maps = error_map
+
+    num_images = data_loader.dataset.__len__()
+    anomaly_scores, _ = torch.max(error_maps.view(num_images, -1), dim=1)
+    anomaly_scores = anomaly_scores.cpu().numpy()
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print('* loss {losses.global_avg:.3f}'
+          .format(losses=metric_logger.loss))
+
+    class_to_idx = data_loader.dataset.class_to_idx
+    targets = torch.tensor(data_loader.dataset.targets)
+    labels = torch.where(targets == class_to_idx['good'], 0, 1)
+    roc_auc = roc_auc_score(labels, anomaly_scores)
+    print('* roc_auc {:.3f}'.format(roc_auc))
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
